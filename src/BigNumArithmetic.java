0d01705ab7a2d240eece8ac018a01190c0e1154a
mhinton2 <mhinton2@mail.umw.edu>
2024-02-22 15:40:00 -0500

    comment

--- src/BigNumArithmetic.java
+++ src/BigNumArithmetic.java
@@ -10,12 +10,8 @@ public class BigNumArithmetic {
                 FileInputStream file = new FileInputStream(args[0]);
                 Scanner scr = new Scanner(file);
                 while(scr.hasNextLine()) {
-<<<<<<< HEAD
                     //System.out.println(scr.nextLine().replaceAll("\\s+", " ").replaceAll("^\\s+", ""));
                     String line = scr.nextLine().replaceAll("\\s+", " ").replaceAll("^\\s+", "");		//replaces extra spaces and lines with 1 space or nothing
-=======
-                    String line = scr.nextLine().replaceAll("\\s+", " ").replaceAll("^\\s+", "");
->>>>>>> branch 'master' of https://github.com/Drvppymar/340_Project2.git
                     String[] items = line.split(" ");
                     if (line.length() > 0) {
                         System.out.println(line);
