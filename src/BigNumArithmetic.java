f7f2d04ffba68f4012d4e7353fd682d0ec2ad133
DrvppyMar <tdaniel@mail.umw.edu>
2024-02-22 21:01:03 -0500

    Added function to take the line string and convert it to an array. Also added the function to take the number and put it into a LList but commented it out because it has a compile issue.

--- src/BigNumArithmetic.java
+++ src/BigNumArithmetic.java
@@ -2,15 +2,28 @@ import java.io.*;
 import java.util.Scanner;
 
 public class BigNumArithmetic {
-    removeLeadingZeroes
-	
+
+    public static String[] lineToArray(String line) {
+        String[] lineArray = line.split(" ");
+        return lineArray;
+    }
+
+    /*
+    public static LList stringToLList(String num) {
+        LList l = new LList();
+        for (int i = 0; i < num.length(); i++) {
+            l.append(num.charAt(i));
+        }
+        return l;
+    }
+     */
+
 	public static void main(String[] args) {
         if (0 < args.length) {
             try {
                 FileInputStream file = new FileInputStream(args[0]);
                 Scanner scr = new Scanner(file);
                 while(scr.hasNextLine()) {
-                    //System.out.println(scr.nextLine().replaceAll("\\s+", " ").replaceAll("^\\s+", ""));
                     String line = scr.nextLine().replaceAll("\\s+", " ").replaceAll("^\\s+", "");		//replaces extra spaces and lines with 1 space or nothing
                     String[] items = line.split(" ");
                     if (line.length() > 0) {
